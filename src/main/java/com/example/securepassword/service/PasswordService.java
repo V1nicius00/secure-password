6900444115a6e53b6529c31b1720f7e063be66f7
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-04-10 12:00:04 -0300

    feat(PasswordService): add methods to validate password requirements

--- src/main/java/com/example/securepassword/service/PasswordService.java
+++ src/main/java/com/example/securepassword/service/PasswordService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class PasswordService {
@@ -12,13 +13,41 @@ public class PasswordService {
         List<String> failures = new ArrayList<>();
 
         validateLength(password, failures);
+        validateUppercase(password, failures);
+        validateLowercase(password, failures);
+        validateNumber(password, failures);
+        validateSpecialChar(password, failures);
 
         return failures;
     }
 
     private void validateLength(String password, List<String> failures) {
         if(password != null && password.length() < 8){
-            failures.add("A senha precisa conter pelo menos 8 caracteres!");
+            failures.add("A senha deve conter pelo menos 8 caracteres.");
+        }
+    }
+
+    private void validateUppercase(String password, List<String> failures){
+        if(!Pattern.matches(".*[A-Z].*", password)){
+            failures.add("A senha deve conter pelo menos 1 letra maiúscula.");
+        }
+    }
+
+    private void validateLowercase(String password, List<String> failures) {
+        if(!Pattern.matches(".*[a-z].*", password)){
+            failures.add("A senha deve conter pelo menos 1 letra minúscula.");
+        }
+    }
+
+    private void validateNumber(String password, List<String> failures) {
+        if(!Pattern.matches(".*[0-9].*", password)){
+            failures.add("A senha deve conter pelo menos 1 número.");
+        }
+    }
+
+    private void validateSpecialChar(String password, List<String> failures) {
+        if(!Pattern.matches(".*[\\W].*", password)){
+            failures.add("A senha deve conter pelo menos 1 caractere especial ex:(!@#$%...).");
         }
     }
 
